fe1e33136a440e9adc68886c23479268438c2065
Karen V <karenvo@usc.edu>
2020-01-21 01:34:32 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,4 +1,11 @@
 let state = {numberOfPresents: 0}
 export function managePresents(state, action){
-
+  switch (action.type){
+    case 'INCREATE_COUNT':
+      return {numberOfPresents: state.count + 1}
+    case 'DECREASE_COUNT':
+      return {numberOfPresents: state.count - 1}
+    default:
+      return state;
+  }
 }
