043c7b1748461a688f40cffdb1bd16bef1121cf9
Karen V <karenvo@usc.edu>
2020-01-21 01:58:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,9 +3,9 @@
 export function manageFriends(state = {friends: []}, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return {}
+      return {friends: [...state.friends, action.friend]}
     case 'REMOVE_FRIEND':
-      return {}
+      return {friends: state.friends.filter(friend => friend.id !== action.id)}
     default:
       return state;
   }
